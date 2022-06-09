37ceed9dc81cb01ccbde8b5419f17dc624447f10
Bacon <Kanoknart_c@cmu.ac.th>
2022-06-09 15:50:30 +0700

    creatw login-page

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import TutorList from "@/views/TutorList";
+import LoginForm from "@/components/LoginForm";
 
 const routes = [
     { path: "/",name: "HomePage" , component: Home },
     { path: "/Tutorlist", component: TutorList },
+    { path: "/login", component: LoginForm },
 ];
 
 const router = createRouter({
