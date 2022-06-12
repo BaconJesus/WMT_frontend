26fa47093baf3f26f1d45ec9333a3bdfabd7e385
Bacon <Kanoknart_c@cmu.ac.th>
2022-06-13 01:05:09 +0700

    Add review box

--- src/views/profilePage.vue
+++ src/views/profilePage.vue
@@ -14,11 +14,47 @@
               <img :src="icon" />
             </div>
             </div>
-            <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">Name Surname</h1>
-            <h3 class="text-gray-600 font-lg text-semibold leading-6">Something Here</h3>
-            <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">Lorem ipsum dolor sit amet
-              consectetur adipisicing elit.
-              Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur non deserunt</p>
+            <h1 class="name text-gray-900 font-bold text-xl leading-8 my-1">Name Surname</h1>
+            <h1 class="rating text-gray-900 font-bold text-xl leading-8 my-1">Rating 4.8⭐</h1>
+          </div>
+          <!-- Student list  -->
+          <div class="bg-white p-3 hover:shadow">
+            <div class="flex items-center space-x-3 font-semibold text-gray-900 text-xl leading-8">
+                        <span class="text-green-500">
+                            <svg class="h-5 fill-current" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                 viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+                            </svg>
+                        </span>
+              <span>Student list</span>
+            </div>
+            <div class="grid grid-cols-3">
+              <div class="text-center my-2">
+                <img class="h-16 w-16 rounded-full mx-auto"
+                     :src="icon"
+                     alt="">
+                <a href="#" class="text-main-color">name1</a>
+              </div>
+              <div class="text-center my-2">
+                <img class="h-16 w-16 rounded-full mx-auto"
+                     :src="icon"
+                     alt="">
+                <a href="#" class="text-main-color">name2</a>
+              </div>
+              <div class="text-center my-2">
+                <img class="h-16 w-16 rounded-full mx-auto"
+                     :src="icon"
+                     alt="">
+                <a href="#" class="text-main-color">name3</a>
+              </div>
+              <div class="text-center my-2">
+                <img class="h-16 w-16 rounded-full mx-auto"
+                     :src="icon"
+                     alt="">
+                <a href="#" class="text-main-color">name4</a>
+              </div>
+            </div>
           </div>
         </div>
         <!-- Right Side -->
@@ -38,36 +74,10 @@
             </div>
             <div class="text-gray-700">
               <div class="grid md:grid-cols-2 text-sm">
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">First Name</div>
-                  <div class="px-4 py-2">Name</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Last Name</div>
-                  <div class="px-4 py-2">Surname</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Gender</div>
-                  <div class="px-4 py-2">Female</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Contact No.</div>
-                  <div class="px-4 py-2">0123456789</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Address</div>
-                  <div class="px-4 py-2">CNX, Thailand</div>
-                </div>
-
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Email.</div>
-                  <div class="px-4 py-2">
-                    <a class="text-blue-800" href="mailto:jane@example.com">name@company.com</a>
-                  </div>
-                </div>
 <!--                <div class="grid grid-cols-2">-->
-<!--                  <div class="px-4 py-2 font-semibold">Birthday</div>-->
-<!--                  <div class="px-4 py-2">Feb 06, 1998</div>-->
+                  <div class="description px-4 py-2 ">Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit.
+                    Reprehenderit, eligendi dolorum sequi illum qui unde aspernatur non deserunt</div>
 <!--                </div>-->
               </div>
             </div>
@@ -89,16 +99,14 @@
                                               d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                     </svg>
                                 </span>
-                  <span class="tracking-wide">Experience</span>
+                  <span class="tracking-wide">Preference</span>
                 </div>
                 <ul class="list-inside space-y-2">
                   <li>
-                    <div class="text-teal-600">Teach at Kumon</div>
-                    <div class="text-gray-500 text-xs">March 2020 - Now</div>
+                    <div class="preference text-teal-600">Preference1</div>
                   </li>
                   <li>
-                    <div class="text-teal-600">Private Tutor</div>
-                    <div class="text-gray-500 text-xs">March 2020 - Now</div>
+                    <div class="preference text-teal-600">Preference2</div>
                   </li>
                 </ul>
               </div>
@@ -114,24 +122,33 @@
                                               d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
                                     </svg>
                                 </span>
-                  <span class="tracking-wide">Education</span>
+                  <span class="tracking-wide">Subject</span>
                 </div>
                 <ul class="list-inside space-y-2">
                   <li>
-                    <div class="text-teal-600">Masters Degree in Oxford</div>
-                    <div class="text-gray-500 text-xs">March 2020 - Now</div>
+                    <div class="subject text-teal-600">Science</div>
                   </li>
                   <li>
-                    <div class="text-teal-600">Bachelors Degree in Copmputer Science</div>
-                    <div class="text-gray-500 text-xs">March 2020 - Now</div>
+                    <div class="subject text-teal-600">Math</div>
                   </li>
                 </ul>
               </div>
             </div>
-            <!-- End of Experience and education grid -->
+            <!-- End of Subject, preferrence -->
           </div>
-          <!-- End of profile tab -->
+          <!--        comment box-->
+          <div class="max-w-lg shadow-md ">
+            <form action="" class="w-full p-4 ">
+              <div class="mb-2">
+                <label for="comment" class="text-lg text-gray-600">Add Review</label>
+                <textarea class="w-full h-20 p-2 border rounded focus:outline-none focus:ring-gray-300 focus:ring-1"
+                          name="comment" placeholder=""></textarea>
+              </div>
+              <button class="px-3 py-2 text-sm text-blue-100 bg-blue-600 rounded">Submit</button>
+            </form>
+          </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -139,7 +156,6 @@
 
 </template>
 
-<!--<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer>-->
 <script>
 export default {
   name: "profilePage",
